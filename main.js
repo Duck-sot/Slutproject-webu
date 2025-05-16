ae19bd7f8b7d4bcf85035da071d93653674747de
Duck-sot <raja07001@utb.vaxjo.se>
2025-05-16 10:54:09 +0200

    gg

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const root = document.documentElement;
 
-const cmc = getComputedStyle(root).getPropertyValue('--bColor')
 
 function ThemeB(){
-    if(cmc == cmc){
+    const cmc = getComputedStyle(root).getPropertyValue('--bColor')
+    if(cmc == '#f2f3f4'){
         root.style.setProperty('--bColor','#1E2952');
         root.style.setProperty('--sColor','#000');
         root.style.setProperty('--inText','#72A0C1');
         root.style.setProperty('--mText','#f2f3f4'); 
     }
     else{
-        root.style.setProperty('--bColor','#F1E9DB');
+        root.style.setProperty('--bColor','#f2f3f4');
         root.style.setProperty('--sColor','#7C8483');
         root.style.setProperty('--inText','#912F56');
         root.style.setProperty('--mText','#000');
